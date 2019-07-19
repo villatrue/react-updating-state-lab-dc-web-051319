d017f29edd1d203d1d412642ee23dfc0187e9a38
villatrue <scarter0313@gmail.com>
2019-07-18 22:03:50 -0400

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,55 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger  extends React.Component{
+
+    constructor(){
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          }
+    }
+
+    changeBitrate = () =>{
+        this.setState(prevState => ({
+            settings: {                   // object that we want to update
+                ...prevState.settings,    // keep all other key-value pairs
+                bitrate: 12       // update the value of specific key
+            }
+        }))
+    }
+
+    changeRes = () =>{
+        this.setState(oldState=>({
+            settings:{
+                    ...oldState.settings,
+                    video:{
+                    resolution: "720p"
+                    }  
+            }
+        }))
+    }
+
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.changeBitrate} className="bitrate">{this.state.settings.bitrate}</button>
+                <button className="resolution" onClick={this.changeRes}>{this.state.settings.video.resolution}</button>
+            </div>
+        )
+    }
+}
+
+// Create a button with the class 'bitrate'. Clicking this 
+// button changes the settings.bitrate state property to 12.
+
+// Create a button with the class 'resolution'. 
+// Clicking this button changes the settings.video.resolution state property to '720p'.
